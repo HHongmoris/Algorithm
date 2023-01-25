f933a0bb6b1a4ce734947e6c8e10a5ea5652f2bf
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-01-26 01:39:44 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 13104 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		int[] A = new int[T];
+		int[] B = new int[T];
+		int[] sum = new int[T];
+		
+		for(int i=0; i<T; i++) {
+			A[i] = sc.nextInt();
+			B[i] = sc.nextInt();
+			sum[i] = A[i] + B[i];
+			
+			System.out.println("Case #"+(i+1)+": "+sum[i]);
+		}
+	}
+}
