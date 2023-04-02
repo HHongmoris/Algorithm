d11f9b5831bf7fd8af1eb86ae31f8525e2e5c828
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-04-02 23:51:22 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 543 ms, Memory: 140,280 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Solution {
-	static int[] p;	//대표 저장할 배열
+	static class Edge implements Comparable<Edge>{
+		int st;
+		int ed;
+		double w;
+		
+		public Edge(int st, int ed, double w) {
+			this.st = st;
+			this.ed = ed;
+			this.w = w;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Double.compare(this.w, o.w);
+		}
+	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -26,77 +44,59 @@ public class Solution {
 			//환경 부담 세율 입력
 			double E = sc.nextDouble();
 			
-			//섬들의 좌표를 활용하여 정점과 간선의 배열로 새롭게 선언
-			//섬들을 모두 잇는 간선의 개수 : (Nx(N-1))/2
-			int idx = 0;
+			//인접 리스트
+			List<Edge>[] adjList = new ArrayList[N];
+			
+			for(int i=0; i<N; i++) {
+				adjList[i] = new ArrayList<>();
+			}
+
 			int road = (N*(N-1))/2;
-			double[][] edges = new double[road][3];
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
 					if(i<j) {
-						edges[idx][0] = i;	//첫번째 정점
-						edges[idx][1] = j;	//이어지는 정점
-						edges[idx][2] = (Math.pow(island[i][0]-island[j][0], 2)+Math.pow(island[i][1]-island[j][1], 2));
-						idx++;
+						int A = i;	//첫번째 정점
+						int B = j;	//이어지는 정점
+						//가중치
+						double W = (Math.pow(island[i][0]-island[j][0], 2)+Math.pow(island[i][1]-island[j][1], 2));
+						
+						adjList[A].add(new Edge(A, B, W));
+						adjList[B].add(new Edge(B, A, W));
+						
 					}
 				}
 			}
 			
-			//크루스칼 1단계 : 간선 정렬(오름차순)
-			Arrays.sort(edges, new Comparator<double[]>() {
-
-				@Override
-				public int compare(double[] o1, double[] o2) {
-					return Double.compare(o1[2], o2[2]);
-				}
-			});
+			//방문처리
+			boolean[] visited = new boolean[N];
 			
-			//크루스칼 2단계
-			// - V-1개의 간선을 뽑는데, 사이클이 발생 안하는 것들만 뽑기
-			p = new int[N];
-			for(int i=0; i<N; i++){
-				makeset(i);
+			PriorityQueue<Edge> pq = new PriorityQueue<>();
+			
+			//시작 정점 뽑고 시작
+			visited[0] = true;
+			
+			pq.addAll(adjList[0]);
+			
+			//뽑은 정점 개수(시작점 뽑고 시작해서 1로 시작)
+			int pick = 1;
+			double ansRoad = 0;
+			
+			//pick < N 이면 계속 뽑기
+			while(pick != N) {
+				Edge e = pq.poll();
+				if(visited[e.ed]) continue; //연결된 정점 뽑았으면 다음으로
+				
+				ansRoad += e.w;		//가중치 더하기
+				pq.addAll(adjList[e.ed]);	//pq에 연결된 정점 정보 추가
+				visited[e.ed] = true;		//방문처리!
+				pick++;		//뽑은 정점 개수 카운트
 			}
 			
-			double ansRoad = 0;	//최소 길이 저장할 곳
-			int pick = 0;		//뽑은 간선 수
-			
-			for(int i=0; i<road; i++) {
-				//i 번째 간선 체크
-				int x = (int) edges[i][0];
-				int y = (int) edges[i][1];
-				
-				//x와 y의 대표가 다르면 사이클 아님
-				if(findset(x) != findset(y)) {
-					union(x, y);
-					ansRoad += edges[i][2];
-					pick++;
-				}
-				
-				if(pick == N-1) break;
-			}
-			
+			//환경 부담금 계산
 			double ans = E*ansRoad;
 			System.out.println("#"+t+" "+Math.round(ans));
 			
 		}
 		
 	}
-	
-	//메서드 만들기
-	static int findset(int x) {
-		if(x != p[x]) {
-			p[x] = findset(p[x]);
-		}
-		return p[x];
-	}
-	
-	static void makeset(int x) {
-		p[x] = x;
-	}
-	
-	static void union(int x, int y) {
-		p[findset(y)] = findset(x);
-	}
-	
 }
