a5fb1656a1b52928c8282809a139a6721e12c2a5
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-04-05 16:40:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 17572 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,57 +1,68 @@
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int K;
-    static boolean[] visited;
-    static Queue<Integer> queue;
+	static int N;
+	static int K;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	//방문처리용
+	static boolean[] visited;
+	//작업할 큐
+	static Queue<Integer> queue;
+	static int time;
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		K = sc.nextInt();
 
-        N = sc.nextInt();
-        K = sc.nextInt();
-        visited = new boolean[100001];
-        queue = new LinkedList<>();
+		visited = new boolean[100001];
+		queue = new LinkedList<>();
+		
+		System.out.println(BFS_soomba(N));
+		
+	}
+	
+	static int BFS_soomba(int v) {
+		queue.add(v);
+		visited[v] = true;
+		time = 0;
+		
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			
+			for(int i=0; i<size; i++) {
+				
+				int next = queue.poll();
+				if(next==K) {
+					return time;
+				}
 
-        int time = BFS(N);
-        System.out.println(time);
-    }
-
-    static int BFS(int start) {
-        queue.add(start);
-        visited[start] = true;
-        int time = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                int current = queue.poll();
-
-                if (current == K) {
-                    return time;
-                }
-
-                if (current - 1 >= 0 && !visited[current - 1]) {
-                    visited[current - 1] = true;
-                    queue.add(current - 1);
-                }
-                if (current + 1 <= 100000 && !visited[current + 1]) {
-                    visited[current + 1] = true;
-                    queue.add(current + 1);
-                }
-                if (2 * current <= 100000 && !visited[2 * current]) {
-                    visited[2 * current] = true;
-                    queue.add(2 * current);
-                }
-            }
-
-            time++;
-        }
-
-        return time;
-    }
+				if(next-1>=0 && next-1<=100000 && !visited[next-1]) {
+					visited[next-1] = true;
+					queue.add(next-1);
+				}
+				if(next+1>=0 && next+1<=100000 && !visited[next+1]) {
+					visited[next+1] = true;
+					queue.add(next+1);
+				
+				}
+				if(next*2>=0 && next*2<=100000 && !visited[next*2]) {
+					visited[next*2] = true;
+					queue.add(next*2);
+				}
+			}
+			
+			time++;
+			
+		}
+		
+		return time;
+	}
+	
+	
 }
