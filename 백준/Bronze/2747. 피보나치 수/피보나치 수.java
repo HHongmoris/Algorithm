7c667cb37595140b7ea79be5245b06bb0944dd59
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-04-07 09:43:57 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//n 입력
+		int N = sc.nextInt();
+		
+		int a = 0;
+		int b = 1;
+		int c = 0;;
+		int cnt = 1;
+		if(N == 1) {
+			System.out.println(1);
+		}else {
+			while(cnt<N) {
+				++cnt;
+				c = a+b;
+				a = b;
+				b = c;
+			}
+			
+			System.out.println(c);
+			
+		}
+		
+	}
+}
