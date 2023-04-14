3ad99f888e67bc81eeb673363309e27cef19f343
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-04-14 18:42:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 116 ms, Memory: 12836 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,22 +5,29 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		int cnt = 0;
 		
-		while(true) {
-			if(N%5 == 0) {
-				cnt += N/5;
-				System.out.println(cnt);
+		int n = N/5;
+		
+		if(N<8 && N%3!=0 && N%5!=0) {
+			System.out.println(-1);
+		}else {
+			switch (N%5) {
+			case 0:
+				System.out.println(n);
 				break;
-			}else if(N%5 != 0) {
-				N = N-3;
-				cnt += 1;
-			}
-			
-			if(N < 0){
-				System.out.println(-1);
+			case 1:
+				System.out.println(n+1);
 				break;
-			}
+			case 2:
+				System.out.println(n+2);
+				break;
+			case 3:
+				System.out.println(n+1);
+				break;
+			case 4:
+				System.out.println(n+2);
+				break;
+			}			
 		}
 		
 	}
