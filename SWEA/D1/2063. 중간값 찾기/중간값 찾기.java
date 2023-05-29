45fc1b4a28de3135ce858ac15de8dc7df8945fd3
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-05-29 20:21:32 +0900

    [D1] Title: 중간값 찾기, Time: 143 ms, Memory: 20,224 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -12,8 +12,27 @@ public class Solution {
 		for(int i=0; i<N; i++) {
 			nums[i] = sc.nextInt();
 		}
-		// 크기 순으로 정렬하기
-		Arrays.sort(nums);
+		
+		//크기 순으로 정렬
+		//1. 맨 앞 숫자부터 맨 뒤까지 탐색하며 최소값 찾는다.
+		//2. 최소값이 맨 앞에 오도록 swap
+		//3. 인덱스를 0부터 순차적으로 올라가며 탐색하고 위 방법을 반복
+		for(int i=0; i<N-1; i++) {
+			//idx를 시작점인 i로 초기화
+			int idx = i;
+			for(int j=i+1; j<N; j++) {
+				//뒤 숫자가 더 작을 경우 그 인덱스 값을 idx로 초기화
+				if(nums[idx]>nums[j]) {
+					idx = j;
+				}
+			}
+			//swap
+			int temp = nums[idx];
+			nums[idx] = nums[i];
+			nums[i] = temp;
+		}
+		
+		
 		//중간값이기 때문에 N/2번째 수 출력
 		System.out.println(nums[N/2]);
 	}
