48a522cbdf36783ac4cd8f9d29ddaadba6813f77
HHongmoris <122426101+HHongmoris@users.noreply.github.com>
2023-05-31 11:23:03 +0900

    [D2] Title: 파리 퇴치, Time: 168 ms, Memory: 25,100 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,49 +1,45 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+//파리퇴치
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//테스트 케이스 수, N, M, 파리 수 배열 입력 받기
+		//테스트케이스 수 입력
 		int T = sc.nextInt();
 		for(int t=1; t<=T; t++) {
+			//파리 있는 영역의 크기 N, 파리채 크기 M 입력
 			int N = sc.nextInt();
 			int M = sc.nextInt();
 			
+			//파리 있는 영역 NxN 행렬 입력
 			int[][] fly = new int[N][N];
-			for(int r=0; r<N; r++) {
-				for(int c=0; c<N; c++) {
-					fly[r][c] = sc.nextInt();
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					fly[i][j] = sc.nextInt();
 				}
 			}
-			//파리 배열에서 MxM 크기에 들어있는 경우를 모두 구하고
-			//그 파리채에 잡힌 파리 수 합을 각각 구하여
-			//그들 중 최댓값을 출력한다.
-			//파리채 배열 생성
-			//파리 배열에서 MxM에 잡힌 값들 합산
-			int[][] flyNums = new int[N-M+1][N-M+1];
-			for(int r=0; r<=N-M; r++) {
-				for(int c=0; c<=N-M; c++) {
-					for(int i=r; i<r+M; i++) {
-						for(int j=c; j<c+M; j++) {
-							flyNums[r][c] += fly[i][j];
+			
+			//파리채 영역인 MxM에 들어있는 파리의 수를 모두 구한 뒤
+			//가장 큰 값을 max로 저장
+			// - fly배열의 인덱스를 0부터 N-M까지 해야 범위 넘어가지 않고 가능
+			int max = 0;
+			for(int i=0; i<=N-M; i++) {
+				for(int j=0; j<=N-M; j++) {
+					//파리채 안에 있는 파리 수 (한칸 갈때마다 초기화 위해 여기에 생성)
+					int sum = 0;	
+					//여기부터 파리채 영역
+					for(int k=i; k<i+M; k++) {
+						for(int l=j; l<j+M; l++) {
+							sum += fly[k][l];
 						}
 					}
+					
+					if(max<sum) max = sum;
 				}
 			}
-			//flyNums 배열에서 최댓값 출력
-			int max = 0;
-			for(int r=0; r<=N-M; r++) {
-				for(int c=0; c<=N-M; c++) {
-					if(max<flyNums[r][c]) {
-						max = flyNums[r][c];
-					}
-				}
-			}
-			System.out.println("#"+t+" "+max);
 			
+			//결과 출력
+			System.out.println("#"+t+" "+max);
 		}
 	}
 }
